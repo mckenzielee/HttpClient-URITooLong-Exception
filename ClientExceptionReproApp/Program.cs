cd114b2341ab6215cb0b97a66e21b28dd9f3066c
McKenzie Owen <mowen@osisoft.com>
2018-07-17 15:59:52 -0700

    Added repro for System.IO.IOException for long request URIs

---
+++ ClientExceptionReproApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientExceptionReproApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string hostUrl = $"http://{Environment.MachineName}:5000";
+            var requestUri = $"api/fake/{new string('1', 30000)}";
+
+            Task.Run(async () =>
+            {
+                Console.WriteLine($"Request URI Length: {requestUri.Length}");
+                using (var client = new HttpClient() { BaseAddress = new Uri(hostUrl) })
+                {
+                    var requestCount = 0;
+                    while (requestCount++ < 10)
+                    {
+                        try
+                        {
+                            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+                            
+                            using (var response = await client.SendAsync(request, CancellationToken.None))
+                            {
+                                await Console.Out.WriteLineAsync($"REQUEST {requestCount} - Response: {(int) response.StatusCode} {response.ReasonPhrase}");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            await Console.Out.WriteLineAsync($"REQUEST {requestCount} - Exception: {ex}");
+                        }
+                    }
+                }
+            });
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Threading;
+
+namespace ServerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cancellationToken.Cancel();
+            };
+
+            IWebHost host = new WebHostBuilder()
+                     .UseUrls($"http://{Environment.MachineName}:5000")
+                     .UseKestrel()
+                     .UseStartup<Startup>()
+                     .Build();
+
+
+            host.RunAsync(cancellationToken.Token).GetAwaiter().GetResult();
+
+        }
+    }
+}
